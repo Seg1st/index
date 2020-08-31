d53f2b3ca9ac022d320223df77b8448f6e91ad18
Andrés Felipe <63387323+Seg1st@users.noreply.github.com>
2020-08-31 02:11:46 -0500

    404 redirecciona al repositorio

---
+++ docs/scripts/DefaultRedir.js
@@ -0,0 +1,4 @@
+/**
+ * @alias Andr7st
+ */
+var site = "https://seg1st.github.io/El_Programador/";let repo_page = "https://github.com/Andr7st/index";let repo_docs = "https://Andr7st.github.io/index/";function redireccionar(){try {window.location=repo_docs;}catch (e) {location.href=repo_docs;console.error("Actualiza navegador");}}setTimeout ("redireccionar()", 0);
