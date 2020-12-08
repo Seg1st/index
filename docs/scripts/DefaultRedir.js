2e5b62992692bf1f37809c61162fbedff707ad6d
Andres Segura <segura1st@outlook.es>
2020-12-08 15:29:25 -0500

    pagina antigua eliminada

--- docs/scripts/DefaultRedir.js
+++
@@ -1,4 +0,0 @@
-/**
- * @alias Andr7st
- */
-var site = "https://seg1st.github.io/El_Programador/";let repo_page = "https://github.com/Andr7st/index";let repo_docs = "https://Andr7st.github.io/index/";function redireccionar(){try {window.location=repo_page;}catch (e) {location.href=repo_page;console.error("Actualiza navegador");}}setTimeout ("redireccionar()", 0);
